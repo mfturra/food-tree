3ccd5fb2a46f43c675030ed82a61cc312902c42d
Matheus Turra <turra.m@northeastern.edu>
2023-08-16 11:18:04 -0400

    Added conditional statement to facilitate usage of json ingestion feature in go app

--- main.go
+++ main.go
@@ -64,62 +64,63 @@ func main() {
 	file_location := "./milk_and_dairy_sample.json"
 	// file_location := "./sample.json"
 	// file_location := "./meat_sample.json"
-
-	jsonFile, err := os.Open(file_location)
-	if err != nil {
-		log.Fatal("Error when opening file:", err)
-		return
-	}
-	defer jsonFile.Close()
-
-	// fmt.Println("Successfully opened file")
-
-	// Read the file content into a []byte slice using os.ReadFile
-	fileContent, err := os.ReadFile(file_location)
-	if err != nil {
-		log.Fatal("Error reading file:", err)
-		return
-	}
-	// fmt.Println(string(fileContent))
-
-	// Unmarshall data into 'payload'
-	var payload []Data
-	err = json.Unmarshal(fileContent, &payload)
-	if err != nil {
-		log.Fatal("Error during Unmarshall():", err)
-	}
-
-	// Print unmarshalled data
-	troubleshoot_json_data := false
-	if troubleshoot_json_data {
-		for _, data := range payload {
-			log.Printf("Ingredient: %s\n", data.IngredientName)
-			log.Printf("Ingredient Quantity: %f\n", data.IngredientQuantity)
-			log.Printf("Quantity Type: %s\n", data.QuantityType)
-			log.Printf("Nutrient Quantity: %d\n", data.NutrientQuantity)
-			log.Printf("Nutrient Quantity Type: %s\n", data.NutrientQuantityType)
-		}
-	}
-
-	for _, data := range payload {
-		insertQuery := `
-				INSERT INTO food_details
-				(ingredient_name, ingredient_quantity, quantity_type, nutrient_quantity, nutrient_quantity_type)
-				VALUES ($1, $2, $3, $4, $5)`
-
-		_, err := db.Exec(
-			insertQuery,
-			data.IngredientName,
-			data.IngredientQuantity,
-			data.QuantityType,
-			data.NutrientQuantity,
-			data.NutrientQuantityType,
-		)
+	if _, err := os.Stat(file_location); err == nil {
+		jsonFile, err := os.Open(file_location)
 		if err != nil {
-			log.Printf("Error inserting data: %v\n", err)
+			log.Fatal("Error when opening file:", err)
 			return
 		}
-		fmt.Println("Succesfully inserted data into SQL DB!")
+		defer jsonFile.Close()
+
+		// fmt.Println("Successfully opened file")
+
+		// Read the file content into a []byte slice using os.ReadFile
+		fileContent, err := os.ReadFile(file_location)
+		if err != nil {
+			log.Fatal("Error reading file:", err)
+			return
+		}
+		// fmt.Println(string(fileContent))
+
+		// Unmarshall data into 'payload'
+		var payload []Data
+		err = json.Unmarshal(fileContent, &payload)
+		if err != nil {
+			log.Fatal("Error during Unmarshall():", err)
+		}
+
+		// Print unmarshalled data
+		troubleshoot_json_data := false
+		if troubleshoot_json_data {
+			for _, data := range payload {
+				log.Printf("Ingredient: %s\n", data.IngredientName)
+				log.Printf("Ingredient Quantity: %f\n", data.IngredientQuantity)
+				log.Printf("Quantity Type: %s\n", data.QuantityType)
+				log.Printf("Nutrient Quantity: %d\n", data.NutrientQuantity)
+				log.Printf("Nutrient Quantity Type: %s\n", data.NutrientQuantityType)
+			}
+		}
+
+		for _, data := range payload {
+			insertQuery := `
+					INSERT INTO food_details
+					(ingredient_name, ingredient_quantity, quantity_type, nutrient_quantity, nutrient_quantity_type)
+					VALUES ($1, $2, $3, $4, $5)`
+
+			_, err := db.Exec(
+				insertQuery,
+				data.IngredientName,
+				data.IngredientQuantity,
+				data.QuantityType,
+				data.NutrientQuantity,
+				data.NutrientQuantityType,
+			)
+			if err != nil {
+				log.Printf("Error inserting data: %v\n", err)
+				return
+			}
+			fmt.Println("Succesfully inserted data into SQL DB!")
+		}
 	}
 
 	// Dynamic Insertion of Data
